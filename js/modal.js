1ff617bd8c03681bfc4617f561e9a8d80a708705
Suciu Nicolae Daniel <daniel.nsuciu@gmail.com>
2024-01-28 19:57:53 +0200

    day3

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,18 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
